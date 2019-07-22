ecfa0d5b86d9c44994f4353e1548a5c614f5b606
obendesmond <obendesmond2@gmail.com>
2019-07-22 05:37:21 +0100

    port issues on fix

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 // check environment using NODE_ENV
 var env = process.env.NODE_ENV || 'development';
 // var env = 'production'
-console.log('desmond this is env: ' + process.env.NODE_ENV)
-    // fetch environment configuration json file
+console.log('desmond this is env: ' + process.env.NODE_ENV);
+console.log('desmond this is port: ' + process.env.PORT);
+// fetch environment configuration json file
 var config = require('./config.json');
 
 var envConfig = config[env];
