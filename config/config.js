78a9b15a97de557837208a387514281f4ad1baf9
obendesmond <obendesmond2@gmail.com>
2019-07-22 03:42:34 +0100

    geting app ready for deployment

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // check environment using NODE_ENV
-// var env = process.env.NODE_ENV || 'development';
-var env = 'production'
+var env = process.env.NODE_ENV || 'development';
+// var env = 'production'
 
 // fetch environment configuration json file
 var config = require('./config.json');
