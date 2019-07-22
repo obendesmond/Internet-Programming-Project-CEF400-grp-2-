7a0dfbf368ed4e7ad9b937650f569b9cc3b43943
obendesmond <obendesmond2@gmail.com>
2019-07-22 06:34:55 +0100

    port issues

--- config/config.js
+++ config/config.js
@@ -1,21 +1,25 @@
 // check environment using NODE_ENV
-var env = process.env.NODE_ENV || 'development';
-console.log('desmond this is env: ' + env);
-console.log('Old Port: ' + env.PORT);
-if (process.env.NODE_ENV == 'production') {
-    env = {
-        PORT: process.env.PORT,
-        MONGODB_URI: "mongodb+srv://desmondperick:desmondperick@cluster0-pc94f.mongodb.net/test?retryWrites=true&w=majority",
-        JWT_SECRET: "SECRET#123",
-        JWT_EXP: "2m"
-    }
-}
+var env = process.env.NODE_ENV || 'production';
 
-console.log('Port Used: ' + env.PORT);
+console.log('desmond this is env: ' + env);
 
 // fetch environment configuration json file
-var config = require('./config.json');
+// var config = require('./config.json');
+// var envConfig = config[env];
+var port = process.env.PORT;
+var envConfig = {
+    PORT: port,
+    MONGODB_URI: "mongodb+srv://desmondperick:desmondperick@cluster0-pc94f.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET: "SECRET#123",
+    JWT_EXP: "3m"
+}
+console.log(envConfig);
 
-var envConfig = config[env];
+Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+});
 
-Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+console.log('FINAL PORT: ' + process.env.PORT);
+console.log('FINAL CON URL: ' + process.env.MONGODB_URI);
+console.log('FINAL JWT_EXP: ' + process.env.JWT_EXP);
+console.log('FINAL JWT_SECRET: ' + process.env.JWT_SECRET);
--- models/db.js
+++ models/db.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
 console.log('Making mongodb database connection....')
-//settings
-var settings = { 
+    //settings
+var settings = {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true
 }
 
-mongoose.connect(process.env.MONGODB_URI, settings,(err) => {
-    if (!err) { console.log('MongoDB connection succeeded.'); }
-    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+const url = "";
+
+mongoose.connect(process.env.MONGODB_URI, settings, (err) => {
+    if (!err) { console.log('MongoDB connection succeeded.'); } else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
 
 // include all the models
